8deb8a2b999a73812cdabee98b2042be45b39e70
Savio-G <savio052003@gmail.com>
2023-08-15 14:33:32 +0600

    fetched the first api from server to client

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
+const homePage = require('./Data/homeData.json')
+var cors = require('cors')
 const port = process.env.PORT || 5000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('SavoryFy server')
+})
+
+app.get('/homePage', (req, res) => {
+  res.send(homePage)
 })
 
 app.listen(port, () => {
